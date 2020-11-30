97062e9bc9e5a95311a9ab42305ef98b6d0a634b
bitcamp <bitcamp@DESKTOP-6REN0OO>
2020-11-30 09:17:49 +0900

    06.commit

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,130 @@
+package com.model2.mvc.web.product;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
+
+@Controller
+public class ProductController {
+
+	
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+
+	public ProductController() {
+		System.out.println(this.getClass());
+	}
+	
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	
+	
+	@RequestMapping("/addProductView.do")
+	public String addProductView()throws Exception{
+		
+		System.out.println("/addProductView.do Start...");
+		
+		return "redirect:/product/addProductView.jsp" ;
+	}
+	
+	@RequestMapping("/addProduct.do")
+	public String addProduct(@ModelAttribute("product")Product product)throws Exception{
+		
+		System.out.println("/addProduct.do Start...");
+		productService.addProduct(product);
+		
+		return "forward:/product/addProduct.jsp";
+		
+	}
+	
+	@RequestMapping("/getProduct.do")
+	public String getProduct(@RequestParam("prodNo")int prodNo, Model model)throws Exception{
+	
+		System.out.println("/getProduct.do Start...");
+		
+		Product product = productService.getProduct(prodNo);
+		System.out.println("°´Ã¼È®ÀÎ:"+product);
+		
+		model.addAttribute("product", product);
+	
+		return  "forward:/product/getProduct.jsp";
+	
+	}
+	
+	@RequestMapping("/updateProductView.do")
+	public String updateProductView(@RequestParam("prodNo")int prodNo, Model model )throws Exception{
+		
+		System.out.println("/updateProductView.do Start...");
+		
+		Product product = productService.getProduct(prodNo);
+		model.addAttribute("product", product);
+		
+		return "forward:/product/updateProductView.jsp";
+	}
+		
+		@RequestMapping("/updateProduct.do")
+		public String updateProduct(@ModelAttribute("product")Product product)throws Exception{
+			
+			System.out.println("/updateProduct.do Start...");
+			
+			productService.updateProduct(product);
+			
+			System.out.println("µð¹ö±ë:"+product);
+			
+			
+	
+		
+				return "redirect:/getProduct.do?prodNo="+product.getProdNo()+"&menu=manage";
+		}
+			
+				
+			
+	
+		
+		
+		
+		@RequestMapping("/listProduct.do")
+		public String listProduct(@ModelAttribute("search")Search search, Model model, HttpServletRequest request)throws Exception{
+			
+			System.out.println("/listProduct.do Start...");
+			
+			if(search.getCurrentPage()==0) {
+				search.setCurrentPage(1);
+			}
+			search.setPageSize(pageSize);
+			
+			Map<String, Object> map = productService.getProductList(search);
+			
+			Page resultPage = new Page(search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+			
+			model.addAttribute("list", map.get("list"));
+			model.addAttribute("resultPage", resultPage);
+			model.addAttribute("search", search);
+			
+			return "forward:/product/listProduct.jsp";
+			
+		
+	}
+}
---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
@@ -0,0 +1,158 @@
+package com.model2.mvc.service.product.test;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductDAO;
+import com.model2.mvc.service.product.ProductService;
+
+import junit.framework.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/context-common.xml",
+															   	"classpath:config/context-aspect.xml",
+															   	 "classpath:config/context-mybatis.xml",
+															   	"classpath:config/context-transaction.xml"})																
+public class ProductServiceTest {
+
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	
+	
+	
+	//@Test
+	public void testAddProduct()throws Exception{
+		
+		
+		Product product = new Product();
+		product.setProdName("k9");
+		product.setProdDetail("µÓ±ﬁº¯¿ß");
+		product.setManuDate("¿∞¿œƒ•±∏");
+		product.setPrice(600000);
+		product.setFileName("123");
+		
+		productService.addProduct(product);
+		
+		System.out.println(product);
+		
+		product = productService.getProduct(10015);
+		
+		Assert.assertEquals("k9", product.getProdName());
+		Assert.assertEquals("µÓ±ﬁº¯¿ß", product.getProdDetail());
+		Assert.assertEquals("¿∞¿œƒ•±∏", product.getManuDate());
+		Assert.assertEquals(600000, product.getPrice());
+		Assert.assertEquals("123", product.getFileName());
+	}
+		
+	//@Test
+	public void testGetProduct()throws Exception{
+		
+		Product product = new Product();
+		
+		product = productService.getProduct(10015);
+		
+		System.out.println(product);
+		
+		Assert.assertEquals("k9", product.getProdName());
+		Assert.assertEquals("µÓ±ﬁº¯¿ß", product.getProdDetail());
+		Assert.assertEquals("¿∞¿œƒ•±∏", product.getManuDate());
+		Assert.assertEquals(600000, product.getPrice());
+		Assert.assertEquals("123", product.getFileName());
+		
+		}
+		
+	//@Test
+	public void testUpdateProduct() throws Exception{
+		
+		Product product  = productService.getProduct(10015);
+		System.out.println("»Æ¿Œ:"+product);
+		//Assert.assertNotNull(product);
+		
+//		Assert.assertEquals("k9", product.getProdName());
+//		Assert.assertEquals("µÓ±ﬁº¯¿ß", product.getProdDetail());
+//		Assert.assertEquals("¿∞¿œƒ•±∏", product.getManuDate());
+//		Assert.assertEquals(600000, product.getPrice());
+		
+		product.setProdName("∏¿µøªÍ");
+		
+		System.out.println("»Æ¿Œ1:"+product);
+		product.setProdDetail("≤ŸøÔ∏¿");
+		
+		System.out.println("»Æ¿Œ2:"+product);
+		
+		product.setManuDate("¥Á¿œ∆¢±Ë");
+		
+		System.out.println("»Æ¿Œ3:"+product);
+		
+		product.setPrice(3000);
+		
+		System.out.println("»Æ¿Œ4:"+product);
+		
+		productService.updateProduct(product);
+		
+		System.out.println("»Æ¿Œ5:"+product);
+		
+		product = productService.getProduct(10016);
+		System.out.println("»Æ¿Œ6:"+product);
+		
+		System.out.println(product);
+		
+		Assert.assertEquals("∏¿µøªÍ", product.getProdName());
+		Assert.assertEquals("≤ŸøÔ∏¿", product.getProdDetail());
+		Assert.assertEquals("¥Á¿œ∆¢±Ë", product.getManuDate());
+		Assert.assertEquals(3000, product.getPrice());
+	}
+	
+	
+	//@Test
+	public void testGetProductListByProdName()throws Exception{
+		
+		Search search = new Search();
+		search.setCurrentPage(1);
+		search.setPageSize(3);
+		search.setSearchCondition("0");
+		search.setSearchKeyword("¿∫º∑");
+		Map<String, Object> map = productService.getProductList(search);
+		
+		List<Object> list = (List<Object>)map.get("list");
+		Assert.assertEquals(1, list.size());
+		
+		System.out.println(list);
+	}
+	
+	@Test
+	 public void testGetProductListByPrice() throws Exception{
+		 
+		 	Search search = new Search();
+		 	search.setCurrentPage(3);
+		 	search.setPageSize(3);
+		 	search.setSearchCondition("1");
+		 	search.setSearchKeyword("11");
+		 	Map<String,Object> map = productService.getProductList(search);
+		 	
+		 	List<Object> list = (List<Object>)map.get("list");
+		 	Assert.assertEquals(1, list.size());
+		 	
+		 	System.out.println(list);
+		
+		 }	 
+	}
+
+	
+
+		
+	
+	
+		
+	
+
